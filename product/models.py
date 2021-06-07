ef0e14eede2a61fb0745a514cdde087c391502c6
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-06-07 13:40:58 +0500

    models and serializers done

--- product/models.py
+++ product/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    brand = models.ForeignKey(Brand, on_delete=models.DO_NOTHING)
+    slug = models.SlugField()
+    price = models.FloatField()
+    size = models.CharField(max_length=255)
+    stock_count = models.PositiveIntegerField()
+
+
