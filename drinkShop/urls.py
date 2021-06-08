b92194b12ca3168f4700aabc7b4909a76148d5ee
ShuhratX <shuhratxayitov@gmail.com>
2021-06-08 19:18:26 +0500

    django admin dashboard customize

--- drinkShop/urls.py
+++ drinkShop/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.index_title = "Xush kelibsiz"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
