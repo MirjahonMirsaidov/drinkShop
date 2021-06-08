83c6539b2837ea7a30b2fbf8d5df2efc44568156
ShuhratX <shuhratxayitov@gmail.com>
2021-06-08 18:54:15 +0500

    django admin dashboard customize

--- product/admin.py
+++ product/admin.py
@@ -14,9 +14,9 @@ admin.site.unregister(Theme)
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'view_products_link')
+    list_display = ('id', 'name', 'view_products_link')
     search_fields = ('name',)
-
+    list_display_links = ('name',)
     def view_products_link(self, obj):
         count = obj.product_set.count()
         url = (
@@ -32,9 +32,9 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'view_products_link')
+    list_display = ('id', 'name', 'view_products_link')
     search_fields = ('name',)
-
+    list_display_links = ('name',)
     def view_products_link(self, obj):
         count = obj.product_set.count()
         url = (
@@ -47,18 +47,23 @@ class BrandAdmin(admin.ModelAdmin):
     view_products_link.short_description = "Products"
 
 
-class ProductImageAdmin(admin.StackedInline):
+class ProductImageAdmin(admin.TabularInline):
     model = Image
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageAdmin]
-    list_display = ('id', 'title', 'category', 'brand', 'price', 'size', 'stock_count')
+    list_display = ('id', 'title', 'category', 'brand', 'price', 'size', 'stock_count', 'created_at')
+    list_display_links = ('title',)
     list_filter = ('created_at', 'price', 'category', 'brand')
     list_editable = ('price', 'stock_count')
     search_fields = ('title', 'brand__name', 'category__name')
     list_per_page = 5
+    raw_id_fields = ("category",)
+    save_on_top = True
+    save_as = True
+
 
     class Meta:
         model = Product
@@ -66,5 +71,6 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Image)
 class ProductImageAdmin(admin.ModelAdmin):
-    list_display = ('product', 'image', 'image_tag', )
+    list_display = ('id', 'product', 'image', 'image_tag', )
     readonly_fields = ('image_tag',)
+    list_display_links = ('product',)
