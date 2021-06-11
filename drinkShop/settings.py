a10edf9e418eb5132e5b06dad49e4c0c68311fd3
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-06-11 16:31:05 +0500

    model translation and rosetta

--- drinkShop/settings.py
+++ drinkShop/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'rosetta',  # manage translations in the django admin
 
 
 
@@ -167,3 +168,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# rosetta Settings
+ROSETTA_MESSAGES_PER_PAGE = 100000
