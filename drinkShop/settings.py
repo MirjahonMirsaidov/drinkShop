5b3af25dda218befd0f223b9d9c5586577a53ab2
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-06-15 14:28:25 +0500

    new template

--- drinkShop/settings.py
+++ drinkShop/settings.py
@@ -37,9 +37,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # custom admin interface
-    # 'jazzmin',
-    'adminlte3',
-    'adminlte3_theme',
+    'jazzmin',
+    # 'adminlte3',
+    # 'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
